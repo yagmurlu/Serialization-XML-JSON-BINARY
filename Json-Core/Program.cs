319010913af1bcbd7a06f1859516cf21f6ac631f
yagmurlu <mavibulut0106@gmail.com>
2021-05-30 00:24:59 +0300

    Json Serialize With .Net Core

---
+++ Json-Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+namespace Json_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human()
+            {
+                Name = "Jabal",
+                Age = 30
+            };
+            string json = JsonSerializer.Serialize<Human>(human);
+            Console.WriteLine(json);
+            Human deserialized = JsonSerializer.Deserialize<Human>(json);
+            Console.WriteLine(deserialized.Name+":"+deserialized.Age);
+        }
+    }
+}
